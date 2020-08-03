b668989f0f88de2d4399055adc71f0d0d21ead2d
Galih Anggi Satriawan <satriawan.galih99@gmail.com>
2020-08-03 11:24:37 +0700

    update gradle

--- app/src/main/java/id/shobrun/ukmmobile/utils/Constants.kt
+++ app/src/main/java/id/shobrun/ukmmobile/utils/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val LOCAL_URL = "http://192.168.43.143:5000/"
         const val DEV_URL="https://ukm-backend.herokuapp.com/"
-        const val API_URL = LOCAL_URL+"api/v0/"
+        const val API_URL = DEV_URL+"api/v0/"
 
         const val HELP_URL = LOCAL_URL+"help"
     }
